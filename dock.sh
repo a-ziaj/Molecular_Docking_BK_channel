927e13f87e34ebf3aee7a43ae80ae13688176d61
a-ziaj <alicja.ziajowska@gmail.com>
2026-03-31 00:05:04 +0200

    updated protein, gridbox amd added dockign of 4 ligands

--- dock.sh
+++ dock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+exec > >(tee -a run.log) 2>&1 #add log
 #######generate job ID
 #add this if you want to run the script multiple times so it doesn't overwrite things
 #job_id=$(printf "%04d" $(( RANDOM % 10000 )))
@@ -59,19 +59,30 @@ do
 	#vina
         vina --receptor protein.pdbqt --ligand ${CAS_ID}.pdbqt  --config config.txt  --exhaustiveness=32 --out ${CAS_ID}_vina_1.pdbqt > ${CAS_ID}_vina_results_1.txt
 	echo -e "Docked one ${CAS_ID} molecule (vina), saved as ${CAS_ID}_vina_1.pdbqt and ${CAS_ID}_vina_results_1.txt"
+	date
 	#vinardo
 	vina --receptor protein.pdbqt --ligand ${CAS_ID}.pdbqt  --config config.txt  --exhaustiveness=32 --out ${CAS_ID}_vinardo_1.pdbqt --scoring vinardo > ${CAS_ID}_vinardo_results_1.txt
 	echo -e "Docked one ${CAS_ID} molecule (vinardo), saved as ${CAS_ID}_vinardo_1.pdbqt and ${CAS_ID}_vinardo_results_1.txt"
-
+	date
 
 	#2 lignads
 	#vina
 	vina --receptor protein.pdbqt --ligand ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt  --config config.txt  --exhaustiveness=32 --out ${CAS_ID}_vina_2.pdbqt > ${CAS_ID}_vina_results_2.txt
 	echo -e "Docked two ${CAS_ID} molecules (vina), saved as  ${CAS_ID}_vina_2.pdbqt and ${CAS_ID}_vina_results_2.txt"
+	date
 	#vinardo
 	vina --receptor protein.pdbqt --ligand ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt  --config config.txt  --exhaustiveness=32 --out ${CAS_ID}_vinardo_2.pdbqt --scoring vinardo > ${CAS_ID}_vinardo_results_2.txt
 	 echo -e "Docked two ${CAS_ID} molecules (vinardo), saved as ${CAS_ID}_vinardo_2.pdbqt and ${CAS_ID}_vinardo_results_2.txt"
-	#date
+	date
+	
+        #4 lignads
+        #vina
+	vina --receptor protein.pdbqt --ligand ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt  --config config.txt  --exhaustiveness=32 --out ${CAS_ID}_vina_4.pdbqt > ${CAS_ID}_vina_results_4.txt
+	echo -e "Docked four ${CAS_ID} molecules (vina), saved as  ${CAS_ID}_vina_4.pdbqt and ${CAS_ID}_vina_results_4.txt"
+        date                                                                        #vinardo
+	vina --receptor protein.pdbqt --ligand ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt ${CAS_ID}.pdbqt  --config config.txt  --exhaustiveness=32 --out ${CAS_ID}_vinardo_4.pdbqt --scoring vinardo > ${CAS_ID}_vinardo_results_4.txt
+         echo -e "Docked four ${CAS_ID} molecules (vinardo), saved as ${CAS_ID}_vinardo_4.pdbqt and ${CAS_ID}_vinardo_results_4.txt"
+        date	
 
 ########clean up
 	mv ${CAS_ID}.sdf fetched_sdf/
@@ -85,20 +96,20 @@ do
         mv ${CAS_ID}_vina_results_2.txt vina_results/
         mv ${CAS_ID}_vinardo_2.pdbqt vinardo_results/
         mv ${CAS_ID}_vinardo_results_2.txt vinardo_results/
-	#if [ ! -d "preped_ligands" ]; then
-	#	mkdir -p "preped_ligands"
-	#fi
-	#mv ${CAS_ID}.pdbqt preped_ligands 
+	mv ${CAS_ID}_vina_1.pdbqt vina_results/
+	mv ${CAS_ID}_vina_results_2.txt vina_results/
+	mv ${CAS_ID}_vinardo_2.pdbqt vinardo_results/
+	mv ${CAS_ID}_vinardo_results_2.txt vinardo_results/
+	if [ ! -d "preped_ligands" ]; then
+		mkdir -p "preped_ligands"
+	fi
+	mv ${CAS_ID}.pdbqt preped_ligands 
 
 done < "CAS_IDs.txt"
 echo -e "everything done"
 
 #print time
 date
-
-#####################PROTEIN PREP
-#mk_prepare_receptor.py -i BK_open_6v22_MD_III_2022_v2_autopsf.pdb -o protein -p -v -g \
-#--box_center 0 0 0 --box_size 25 25 50
 
 
 
